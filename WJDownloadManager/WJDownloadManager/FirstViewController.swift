09f3dc36cecd23b80062a545eca7b2690a70fba7
Jerry_ <zhang17xy@126.com>
2015-09-23 16:17:51 +0800

    Demo init

---
+++ WJDownloadManager/WJDownloadManager/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WJDownloadManager
+//
+//  Created by zhangwujian on 15/9/23.
+//  Copyright © 2015年 Z_Jerry. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
